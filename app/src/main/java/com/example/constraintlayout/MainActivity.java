08cfa7f2adccffec367028d9f14fe0dfd9e2711d
sekarputrii <sekarputriretno83@gmail.com>
2021-03-30 16:20:42 +0700

    Activity5

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editTextTextPersonName , editTextTextPassword;
+    EditText editTextTextPersonName, editTextTextPassword;
     Button button;
     String Username, Password;
 
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 Password = editTextTextPassword.getText().toString();
 
                 //Mengecek apakah isi dari email dan password sudah sama dengan email dan login
-                if(Username.equals("sekarputri@gmail.com") && Password.equals("Sekar83")) {
+                if (Username.equals("sekarputri@gmail.com") && Password.equals("Sekar83")) {
                     Toast s = Toast.makeText(getApplicationContext(),
                             "Success, \nUsername : " + Username + "\nPassword : " + Password, Toast.LENGTH_LONG);
                     s.show();
@@ -45,19 +45,15 @@ public class MainActivity extends AppCompatActivity {
                     Intent NewIntent = new Intent(MainActivity.this, MainActivity2.class);
                     NewIntent.putExtras(b);
                     startActivity(NewIntent);
-                }
-                else if(!Username.equals("sekarputri@gmail.com") && Password.equals("Sekar83"))
-                {
+                } else if (!Username.equals("sekarputri@gmail.com") && Password.equals("Sekar83")) {
                     Toast s = Toast.makeText(getApplicationContext(),
                             "Login Failed (Wrong Username Entered)", Toast.LENGTH_LONG);
                     s.show();
-                }
-                else if (Username.equals("sekarputri@gmail.com") && !Password.equals("Sekar83")) {
+                } else if (Username.equals("sekarputri@gmail.com") && !Password.equals("Sekar83")) {
                     Toast s = Toast.makeText(getApplicationContext(),
                             "Login Failed (Wrong Password Entered)", Toast.LENGTH_LONG);
                     s.show();
-                }
-                else{
+                } else {
                     Toast s = Toast.makeText(getApplicationContext(),
                             "Failed", Toast.LENGTH_LONG);
                     s.show();
@@ -65,16 +61,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public boolean validasiData(){
+
+    public boolean validasiData() {
         //Mengeset email yang benar
         String nameActive = "sekarputri@gmail.com";
         //Mengeset password yang benar
         String passwordActive = "Sekar83";
         //Mengecek apakah isi dari email dan password sudah sama dengan email dan login
-        if(nameActive.equals(editTextTextPersonName.getText().toString()) && passwordActive.equals(editTextTextPassword.getText().toString())){
+        if (nameActive.equals(editTextTextPersonName.getText().toString()) && passwordActive.equals(editTextTextPassword.getText().toString())) {
             return true;
         }
-        return false; 
+        return false;
     }
 
     @Override
@@ -85,16 +82,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.formulir)
-        {
-            Intent intent = new Intent(MainActivity.this, form_baru.class);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.formulir) {
+            Intent intent = new Intent(getApplicationContext(), Home_Activity.class);
             startActivity(intent);
         }
-        else if (item.getItemId() == R.id.EditData)
-        {
-            Intent intent = new Intent(MainActivity.this, Menu.class);
-        }
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 }
